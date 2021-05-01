f6be96e841e27788e3c0bd6e4f230062d43834c6
Saksham Gupta <saksham1319@gmail.com>
2021-05-01 22:49:23 +0530

    action created

---
+++ frontend/src/store.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducers/productReducers.js'
+
+const reducers = combineReducers({ productList: productListReducer })
+
+const initialState = {}
+
+const middleware = [thunk]
+
+const store = createStore(
+    reducers,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware)),
+)
+
+export default store
