4280da439e9991cbae4e5ed11dc59cdb6bcde1f4
Saksham Gupta <saksham1319@gmail.com>
2021-05-16 20:48:13 +0530

    correction

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,25 +1,23 @@
-import React, {useEffect} from 'react'
-import {Link} from 'react-router-dom'
-import {useDispatch, useSelector} from 'react-redux'
-import {row,Col,ListGroup,Image , Form,Button, Card} from 'react-bootstrap'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
-import {addToCart} from '../actions/cartActions'
+import { addToCart } from '../actions/cartActions'
 
+const CartScreen = ({ match, location, history }) => {
+    const productId = match.params.id
 
-const CartScreen = ({match, location, history}) => {
-    const productId=match.params.id
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
 
-    const qty= location.search? Number(location.search.split('=')[1]) : 1
-    
-    const dispatch= useDispatch()
+    const dispatch = useDispatch()
 
-    useEffect(() =>{
-    	if(productId){
-    		dispatch(addToCart(productId, qty))
-    	}
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty))
+        }
     }, [dispatch, productId, qty])
     return <div>Cart</div>
-
 }
 
 export default CartScreen
