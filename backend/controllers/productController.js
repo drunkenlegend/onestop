09ef1aa3f8f6375d0690fc9c1fcd1a8d5efc273f
drunkenlegend <saksham1319@gmail.com>
2021-10-06 12:49:56 +0530

    product list and delete

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,22 +2,34 @@
 import Product from '../models/productModel.js'
 import asyncHandler from 'express-async-handler'
 
-const getProducts =asyncHandler(async(req,res) => {
-	const products=await Product.find({})
-	res.json(products)
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
 })
 
-const getProductById=asyncHandler(async(req,res)=> {
-	const product = await Product.findById(req.params.id)
-        if (product) {
-            res.json(product)
-        } else {
-            res.status(404)
-            throw new Error('Product not Found')
-        }
+const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+        res.json(product)
+    } else {
+        res.status(404)
+        throw new Error('Product not Found')
+    }
 })
 
-export{
-	getProducts,
-	getProductById
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+        await product.remove()
+        res.json({ message: 'Product Removed' })
+    } else {
+        res.status(404)
+        throw new Error('Product not Found')
+    }
+})
+
+export {
+    getProducts,
+    getProductById,
+    deleteProduct
 }
